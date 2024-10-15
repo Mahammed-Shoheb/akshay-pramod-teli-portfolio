acf9d141ee87af78fe174e1a1314b89633f31200
Mahammed Shoheb <mdshoheb135@gmail.com>
2024-10-16 03:44:48 +0530

    remove bujak assosiate roll from work experineince section

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -60,49 +60,7 @@ const data = [
       </ul>
     ),
   },
-  {
-    title: (
-      <>
-        <span className="text-primary">Associate Product Manager Bijak </span>
-        <span className="md:text-2xl">Bangalore</span>
-        <br />
-        <span className="md:text-2xl">Jun 2023 - Present</span>
-      </>
-    ),
-    content: (
-      <ul className="text-neutral-800 dark:text-neutral-200 font-normal mb-8 flex flex-col gap-4 list-disc">
-        <li>
-          Achieved more than 100% increase in app engagement through the
-          implementation of multi-lingual support and the incorporation of clear
-          user action prompts.
-        </li>
-        <li>
-          Implemented and Automated Trade and Loan Management System, enhancing
-          organizations’ ability to manage & scale trades and lending worth 100+
-          Cr, saving 4 FTEs
-        </li>
-        <li>
-          Streamlined specific Trade processes and built an inventory management
-          system, resulting in a 200% increase in overall operational efficiency
-        </li>
-        <li>
-          Drove a 25% boost in DAU/MAU by gamifying the User Interface on the
-          App
-        </li>
-        <li>
-          Created comprehensive reports at both macro and micro levels,
-          analyzing projected and actual returns to assess investment
-          profitability
-        </li>
-        <li>
-          Managed multiple product responsibilities in daily Scrum, release
-          planning, and go-to-market strategies.
-        </li>
-        <li>Led over 50 deployment calls in 1.5 years</li>
-        <li>Managed a high-performing team of 27 members.</li>
-      </ul>
-    ),
-  },
+
   {
     title: (
       <>
